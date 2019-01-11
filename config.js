ec61825030bbd766f663253da4949be4b52ca69a
Eric Carlson <eric.paul.carlson@gmail.com>
2019-01-10 20:09:08 -0800

    switched to mlab db

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/capstone';
+//exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/capstone';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://admin:34excachilles@ds153824.mlab.com:53824/time-flies-db';
 exports.PORT = process.env.PORT || 8080;
 
 exports.JWT_SECRET = process.env.JWT_SECRET;
